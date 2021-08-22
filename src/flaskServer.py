caf2141d80cba1e49decb52bce7f28e1a7db78d5
Mikel Galdos <mikelgaldosv@gmail.com>
2021-08-22 17:58:32 +0200

    routes created for gear and software

--- src/flaskServer.py
+++ src/flaskServer.py
@@ -6,6 +6,14 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     return render_template("index.html")
 
+@app.route("/software/", methods=['POST', 'GET'])
+def software():
+    return render_template("software.html")
+
+@app.route("/gear/", methods=['GET'])
+def gear():
+    return render_template('gear.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
