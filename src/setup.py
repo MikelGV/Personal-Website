2a8d726e96da8d8593546c0c1e34b7e31398b5ee
Mikel Galdos <mikelgaldosv@gmail.com>
2021-08-23 21:54:02 +0200

    things moved

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="Personal-website",
+    version="0.1",
+    description='Flask personal website',
+    author="Mikel Galdos",
+    author_email="mikelgaldosv@gmail.com",
+    url="placeholder",
+    packages=["flaskr"]
+)
