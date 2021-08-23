09612b1012204dc49184f73f77228f204b0d3ed6
Mikel Galdos <mikelgaldosv@gmail.com>
2021-08-23 22:13:59 +0200

    create_app function added

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -1,19 +1,33 @@
-from flask import Flask, app, render_template, request
+from flask import Flask
 
-app = Flask(__name__, static_url_path='/static')
+import os
 
-@app.route("/", methods=['POST', 'GET'])
-def index():
-    return render_template("index.html")
-
-@app.route("/software/", methods=['POST', 'GET'])
-def software():
-    return render_template("software.html")
-
-@app.route("/gear/", methods=['GET'])
-def gear():
-    return render_template('gear.html')
+from flask.templating import render_template
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE= os.path.join(app.instance_path, "flaskr.sqliter"),
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def home():
+        return render_template("index.html")
+    
+    return app
