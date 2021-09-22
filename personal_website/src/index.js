82d2fcc27419ad16c16e66c73d4921b030655edc
Mikel Galdos <mikelgaldosv@gmail.com>
2021-09-22 20:19:02 +0200

    added the AboutMe and PopularProjects classes

--- personal_website/src/index.js
+++ personal_website/src/index.js
@@ -7,9 +7,9 @@ class Header extends React.Component {
 	render() {
 		return (
 			<div>
-				<nav className="navbar navbar-expand-lg fixed-top">
+				<nav id="gobla-nav" className="navbar navbar-expand-lg fixed-top scrolled-nav">
 					<div className="container">
-						<div className="collapse navbar-collapse">
+						<div className="collapse navbar-collapse scrollN unscrolledLink">
 						<ul>
 							<li><a href="/">Home</a></li>
 							<li><a href="#about">About</a></li>
@@ -34,9 +34,66 @@ class Header extends React.Component {
 class Footer extends React.Component {
 	render() {
 		return(
-			<div class="d-flex flex-row align-items-center footer shadow-lg mt-4">
-				<p class="m-auto">Stay Connected</p>
+			<div className="d-flex flex-row align-items-center footer shadow-lg mt-4">
+				<p className="m-auto">Stay Connected</p>
 			</div>
+		)
+	}
+}
+
+class AboutMe extends React.Component {
+	render() {
+		return(
+			<div className="aboutsection">
+				<div id="about" className="content-wrapper">
+					<h1>About me</h1>
+					<p>I've always sought out opportunities and challenges that are meaningful to me. I've never stopped engaging my passion to 
+						help others and solve problems. As a Software developer, I enjoy using my obsessive attention to detail, my unequivocal love for making things, and my
+						mission-driven work ethic to literally change the world. That's why I'm excited to make big impact at a high growth company.
+					</p>
+        		</div>
+			</div>
+		)
+	}
+}
+
+class PopularProjects extends React.Component {
+	render() {
+		return(
+			<div className="popular">
+				<h4>Popular projects</h4>
+				<div>
+					<div>
+						<table class="table">
+							<thead >
+								<tr>
+									<th>Title</th>
+									<th>Description</th>
+									<th>Language</th> 
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<a href="https://github.com/MikelGV/chatMe" className="plainLink">chatMe</a>
+									</td>
+									<td>
+										Chat Application build in django and django-channels as websocket
+									</td>
+									<td>Python</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="https://github.com/MikelGV/SudokuSolver" className="plainLink">SudokuSolver</a>
+									</td>
+									<td>Sudoku game using pygame and using backtracking algorithm to solve the sudoku</td>
+									<td>Python</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+        	</div>
 		)
 	}
 }
@@ -51,10 +108,12 @@ class Main extends React.Component {
 					<div className='center arial'>
 						<h1 className='titleMain'>Mikel Galdos</h1>
 						<h5>
-							"Self-taught software developer through research & development."
+							Self-taught software developer through research & development.
 						</h5>
 					</div>
 				</div>
+				<AboutMe/>
+				<PopularProjects/>
 				<Footer/>
 			</div>
 		)
