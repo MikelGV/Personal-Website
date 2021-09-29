a88fbb623b7ed6d2d302497557d566b21d449504
Mikel Galdos <mikelgaldosv@gmail.com>
2021-09-29 05:15:27 +0200

    added software development to mywork

--- personal_website/src/index.js
+++ personal_website/src/index.js
@@ -72,6 +72,14 @@ function MyWork()  {
 					help others and solve problems. As a Software developer, I enjoy using my obsessive attention to detail, my unequivocal love for making things, and my
 					mission-driven work ethic to literally change the world. That's why I'm excited to make big impact at a high growth company.
 				</p>
+				<div className="col-lg-3">
+					<h5>Software Development</h5>
+					<ul>
+						<li>
+							microservices, APIs, full stack web apps
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	)
