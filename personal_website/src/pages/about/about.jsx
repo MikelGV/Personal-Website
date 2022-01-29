fb92059afef84f782296bd135030a6a93b52621e
MikelGV <mikelgaldosv@gmail.com>
2022-01-29 10:02:38 +0100

    a lot of things added

--- personal_website/src/pages/about/about.jsx
+++ personal_website/src/pages/about/about.jsx
@@ -1,7 +1,12 @@
+import "./about.css"
 export default function AboutMe() {
 	return(
-		<div>
-			<h1>About Me</h1>
+		<div className="about">
+			<img className="aboutImg" src="" alt="" />
+			<div className="aboutWrapper">
+			<h1 className="aboutTitle">About Me</h1>
+			<p className="aboutText">test text</p>
+			</div>
 		</div>
 	)
 }
