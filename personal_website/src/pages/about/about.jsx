2b3d556f72742913165384f71163f3a81bbdde8c
MikelGV <mikelgaldosv@gmail.com>
2022-01-30 00:04:42 +0100

    things

--- personal_website/src/pages/about/about.jsx
+++ personal_website/src/pages/about/about.jsx
@@ -5,7 +5,9 @@ export default function AboutMe() {
 			<h4 className="aboutImg" alt=""></h4>
 			<div className="aboutWrapper">
 			<h1 className="aboutTitle">About Me</h1>
-			<p className="aboutText">test text</p>
+			<div className="aboutRightWrapper">
+			<p className="aboutText">Lorem ipsum dolor sit amet consectetur adipisicing elit.Distinctio rerum repellendus ipsum dolore aliquid exercitationem neque eaque voluptas assumenda, earum nam eligendi ipsa minima, quasi nobis, vitae ex perferendis quae!</p>
+			</div>
 			</div>
 		</div>
 	)
