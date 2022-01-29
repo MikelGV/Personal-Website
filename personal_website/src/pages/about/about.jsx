8209ed7908a76df990df581422de094fbb29dc0d
MikelGV <mikelgaldosv@gmail.com>
2022-01-29 23:14:44 +0100

    things happened

--- personal_website/src/pages/about/about.jsx
+++ personal_website/src/pages/about/about.jsx
@@ -2,7 +2,7 @@ import "./about.css"
 export default function AboutMe() {
 	return(
 		<div className="about">
-			<img className="aboutImg" src="" alt="" />
+			<h4 className="aboutImg" alt=""></h4>
 			<div className="aboutWrapper">
 			<h1 className="aboutTitle">About Me</h1>
 			<p className="aboutText">test text</p>
