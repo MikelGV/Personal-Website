c092d070470a63bd627050237d8ffc7b6a71492d
MikelGV <mikelgaldosv@gmail.com>
2022-04-04 00:39:41 +0200

    home route

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,7 +1,12 @@
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
