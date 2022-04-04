21523d6d3a4d042e83ca5b0d458e5a71189eef38
MikelGV <mikelgaldosv@gmail.com>
2022-04-04 05:12:17 +0200

    container added

--- personal-website/src/components/footer/Footer.jsx
+++ personal-website/src/components/footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+`;
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Container>Footer</Container>
   )
 }
 
