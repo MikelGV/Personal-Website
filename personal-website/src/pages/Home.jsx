ecc81a16da512df86d6e07e50b2cc48f74759e1c
MikelGV <mikelgaldosv@gmail.com>
2022-04-04 05:12:45 +0200

    landing page added

--- personal-website/src/pages/Home.jsx
+++ personal-website/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import TopBar from '../components/topBar/TopBar'
 import SideBar from '../components/sideBar/SideBar'
 import Footer from '../components/footer/Footer'
+import Landing from '../components/landing/Landing'
 
 
 const Home = () => {
   return (
     <div>
       <TopBar/>
+      <Landing/>
       <SideBar/>
       <Footer/>
     </div>
