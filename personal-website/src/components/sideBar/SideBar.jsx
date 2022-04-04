8ec6e37e9c85acd27602fa67b44a35dbc50289e1
MikelGV <mikelgaldosv@gmail.com>
2022-04-04 05:12:35 +0200

    sidebar left side v1 added

--- personal-website/src/components/sideBar/SideBar.jsx
+++ personal-website/src/components/sideBar/SideBar.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import styled from 'styled-components'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const Container = styled.div`
+  width: 40px;
+  position: flex;
+  bottom: 0px;
+  left: 40px;
+  right: auto;
+  z-index: 10;
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  -moz-box-align: center;
+  padding: 0px;
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  padding: 10px;
+  cursor: pointer;
+  color: white;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+`;
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <Container>
+      <Wrapper>
+        <List>
+          <ListItem>
+            <GitHubIcon/>
+          </ListItem>
+          <ListItem>
+            <LinkedInIcon/>
+          </ListItem>
+          <ListItem>
+            <InstagramIcon/>
+          </ListItem>
+        </List>
+      </Wrapper>
+    </Container>
   )
 }
 
