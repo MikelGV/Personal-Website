ee1137704f1261d1398d5f7e88930fc3c4cdf377
MikelGV <mikelgaldosv@gmail.com>
2022-04-06 18:45:13 +0200

    footer v1

--- personal-website/src/components/footer/Footer.jsx
+++ personal-website/src/components/footer/Footer.jsx
@@ -5,9 +5,26 @@ const Container = styled.div`
   display: flex;
 `;
 
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Item = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #ade8f4;
+`;
+
 const Footer = () => {
   return (
-    <Container>Footer</Container>
+    <Container>
+      <Center>
+        <Item>Designed and built by Mikel Galdos.</Item>
+      </Center>
+    </Container>
   )
 }
 
