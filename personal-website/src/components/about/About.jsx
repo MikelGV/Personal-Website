22590003726bfae7411a0b43d6f4ce4441915eeb
MikelGV <mikelgaldosv@gmail.com>
2022-04-10 06:07:01 +0200

    about v3

--- personal-website/src/components/about/About.jsx
+++ personal-website/src/components/about/About.jsx
@@ -8,46 +8,68 @@ const Container = styled.div`
   
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled.div``;
+
+const AboutWrapper = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
 `;
 
 const Title = styled.h1`
+  font-size: 40px;
   color: #ade8f4;
 `;
 
 const Line = styled.hr`
-  border-right: 20vh solid #ade8f4;
+  border-right: 30vh solid #ade8f4;
+  margin-bottom: 10px;
 `;
 
 const Text = styled.span`
   display: flex;
-  max-width: 430px;
+  font-size: 20px;
+  max-width: 420px;
   color: #0096c7;
 `;
 
-const TechnologyWrapper = styled.ul``;
+const TechnologyWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 10px;
+`;
+
+const Technology = styled.ul`
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  grid-gap: 16px;
+`;
 
 const Tech = styled.li`
+  font-size: 20px;
   color: #0096c7;
+  
 `;
 
 const About = () => {
   return (
     <Container>
       <Wrapper>
-        <Title>About</Title>
-        <Line></Line>
-        <Text>Hello! My name is Mikel and I enjoy creating things that live on the internet. My interest in development started back in 2019 when my brother showed me what he was learning and i decided to try it - turns out that i liked it and i learnt HTML & CSS! </Text>
-        <Text>Fast-forward to today, and I've had the privilege to spand my knowledge helping friends build their own projects. My main goal is helping others building websites.</Text>
-        <Text>Here are a few technologies I've been working with recently:</Text>
+        <AboutWrapper>
+          <Title>About</Title>
+          <Line></Line>
+          <Text>Hello! My name is Mikel and I enjoy creating things that live on the internet. My interest in development started back in 2019 when my brother showed me what he was learning and i decided to try it - turns out that i liked it and i learnt HTML & CSS! </Text>
+          <Text>Fast-forward to today, and I've had the privilege to spand my knowledge helping friends build their own projects. My main goal is helping others building websites.</Text>
+          <Text>Here are a few technologies I've been working with recently:</Text>
+        </AboutWrapper>
         <TechnologyWrapper>
-          <Tech>JavaScript (ES6+)</Tech>
-          <Tech>React</Tech>
-          <Tech>NodeJs</Tech>
-          <Tech>Python</Tech>
+          <Technology>
+            <Tech>JavaScript (ES6+)</Tech>
+            <Tech>React</Tech>
+            <Tech>NodeJs</Tech>
+            <Tech>Python</Tech>
+          </Technology>
         </TechnologyWrapper>
       </Wrapper>
     </Container>
