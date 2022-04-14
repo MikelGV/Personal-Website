19010775dcb6b0fd9f3b6ea52c3779464a44b473
MikelGV <mikelgaldosv@gmail.com>
2022-04-14 12:53:22 +0200

    work v4

--- personal-website/src/components/work/Work.jsx
+++ personal-website/src/components/work/Work.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Container = styled.div`
   width: 100%;
-  height: 50vh;
-  margin-top: 30vh;
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
 `;
 
 const Wrapper = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 90vh;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -16,29 +24,18 @@ const Wrapper = styled.div`
 
 const Title = styled.h1`
   color: #48cae4;
+  font-size: 40px;
+  font-weight: 300;
 `;
 
 const Line = styled.hr`
-  border-right: 55vh solid #ade8f4;
+  border-right: 40vh solid #ade8f4;
   margin-bottom: 20px;
 `;
 
-const ProjectWrapper = styled.div`
-  flex: 1;
-  margin: 5px;
-  min-width: 280px;
-  height: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  &:hover ${Container}{
-    opacity: 1;
-  }
-`;
-
 const Project = styled.div`
-  margin-right: 10px;
+  display: flex;
+  border: 10px solid #001233;
 `;
 
 const ProjectImg = styled.img`
@@ -47,15 +44,37 @@ const ProjectImg = styled.img`
   object-fit: cover;
   cursor: pointer;
 `;
+const Info = styled.div`
+  margin-left: 10px;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
 
 const ProjectTitle = styled.h2`
   color: #ade8f4;
+  width: 150px;
+  background-color: rgba(0, 0, 0, 0.01);
 `;
 
-const ProjectTech = styled.ul``;
+const ProjectDesc = styled.span`
+  color: #00b4d8;
+  background-color: rgba(0, 0, 0, 0.01);
+  display: flex;
+  max-width: 350px;
+`;
+
+const ProjectTech = styled.ul`
+  display: grid;
+  grid-template-columns: 125px 1fr;
+  grid-gap: 16px;
+  list-style: none;
+  background-color: rgba(0, 0, 0, 0.01);
+`;
 
 const Tech = styled.li`
-  color: #0096c7;
+  font-size: 15px;
+  color: #48cae4;
+  margin-top: 10px;
+  background-color: rgba(0, 0, 0, 0.01);
 `;
 
 
@@ -63,29 +82,35 @@ const Work = () => {
   return (
     <Container>
       <Wrapper>
-        <Title>Some of the projects i built</Title>
+        <Title>Some  Things I've Built</Title>
         <Line />
-        <ProjectWrapper>
           <Project>
             <ProjectImg src='/assets/E-shop.png' />
-            <ProjectTitle>Social Media</ProjectTitle>
-            <ProjectTech>
-              <Tech>Express</Tech>
-              <Tech>React</Tech>
-              <Tech>Heroku</Tech>
-            </ProjectTech>
+            <Info>
+              <ProjectTitle>Social Media</ProjectTitle>
+              <ProjectDesc>Social Media app that replicates the core Facebook functionalities, like posting photos and following other users. Technologies used:</ProjectDesc>
+              <ProjectTech>
+                <Tech>Express</Tech>
+                <Tech>React</Tech>
+                <Tech>Heroku</Tech>
+              </ProjectTech>
+              <GitHubIcon style={{color: "white", marginLeft: 10, marginTop: 10}}/>
+            </Info>
           </Project>
           <Project>
             <ProjectImg src='/assets/E-shop.png' />
-            <ProjectTitle>E-shop</ProjectTitle>
-            <ProjectTech>
-              <Tech>Express</Tech>
-              <Tech>React</Tech>
-              <Tech>Styled-componets</Tech>
-              <Tech>Heroku</Tech>
-            </ProjectTech>
+            <Info>
+              <ProjectTitle>E-shop</ProjectTitle>
+              <ProjectDesc>Web app for buying and selling clothes. Technologies used:</ProjectDesc>
+              <ProjectTech>
+                <Tech>Express</Tech>
+                <Tech>React</Tech>
+                <Tech>Styled-componets</Tech>
+                <Tech>Heroku</Tech>
+              </ProjectTech>
+              <GitHubIcon style={{color: "white", marginLeft: 10, marginTop: 10}}/>
+            </Info>
           </Project>
-        </ProjectWrapper>
       </Wrapper>
     </Container>
   )
