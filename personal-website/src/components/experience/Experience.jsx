345808b3df4c902dfbb374caa36b467b2dbd71b6
Mikel <mikelgaldosv@gmail.com>
2022-09-27 17:24:29 +0200

    projects moved to projects.jsx

--- personal-website/src/components/experience/Experience.jsx
+++ personal-website/src/components/experience/Experience.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components';
+import Projects from './projects';
 
 
 const Container = styled.div`
@@ -106,7 +107,6 @@ const Experience = () => {
 
   const [isActive, setIsActive] = useState(true);
   const [isActive2, setIsActive2] = useState(false);
-  const [isActive3, setIsActive3] = useState(false);
 
   const toggleHide = () => {
     switch (isActive) {
@@ -146,35 +146,7 @@ const Experience = () => {
             </LeftButton>
           </Left>
           <Right>
-            <Project style={ {display: isActive ? "block" : "none"}}>
-              <RightTitle>Contributed at <a href="https://www.tooljet.com/" target="_blank" style={{textDecoration: "none", color:"#90e0ef"}}>ToolJet</a></RightTitle>
-              <ListWrapper>
-                <Item>What is ToolJet:</Item>
-                <Item>ToolJet is an open-source low-code framework to build and deploy internal tools quickly without much effort from the engineering teams.</Item>
-                <List>
-                  <Item>Worked at <a href="https://github.com/ToolJet/ToolJet/issues/3181" target="_blank" style={{textDecoration:"none", color:"#00b4d8"}}>#3181</a> issue. Default value in text widget deleted.</Item>
-                </List>
-              </ListWrapper>
-            </Project>
-            <Project style={{display: isActive2 ? "block" : "none"}}>
-              <RightTitle>Worked as Freelancer</RightTitle>
-              <TimeStamp>January 2020 - June 2022</TimeStamp>
-              <ListWrapper>
-                <List>
-                  <Item>Providing a range of web development services to numberous clients on a freelance basis; from creating web apps to website builds and rebuilds.</Item>
-                  <Item>Key projects: Build of a E-commerce website (front and back end) using NodeJs and MongoDb</Item>
-                </List>
-              </ListWrapper>
-            </Project>
-            <Project style={{display: isActive3 ? "block" : "none"}}>
-              <RightTitle>Contributed at TEST</RightTitle>
-              <ListWrapper>
-                <List>
-                  <Item>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Item>
-                  <Item>Lorem ipsum dolor sit amet consectetur adipisicing elit. A obcaecati impedit voluptate aliquid. netflix, youtube, twitter.</Item>
-                </List>
-              </ListWrapper>
-            </Project>
+            <Projects/>
           </Right>
         </ExperienceWrapper>
       </Wrapper>
